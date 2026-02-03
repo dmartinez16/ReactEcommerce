02343b62fd86e5552fbcb9c98790a117a5a05b67
Dilan Esnehider Martinez Silva <d.martinez@syc.com.co>
2026-02-03 11:47:16 -0500

    Cambio de tipo para la estructura de producto

--- src/Types/product.ts
+++ src/Types/product.ts
@@ -1,14 +1,8 @@
-export interface Category {
-  id: number;
-  name: string;
-  image: string;
-}
-
 export interface Product {
   id: number;
   title: string;
   price: number;
   description: string;
   image: string;
-  category: Category;
+  category: string;
 }
