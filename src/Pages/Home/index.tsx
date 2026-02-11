dbaa0bf368adf60aa92cbd2f6bf3099ef056d72c
Dilan Esnehider Martinez Silva <d.martinez@syc.com.co>
2026-02-11 15:36:40 -0500

    Buscador para articulos

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,36 +1,33 @@
-import { useEffect, useState } from "react"
+import { useShoppingCar } from "../../Hooks/useShoppingCar";
 import Layout from "../../Components/Layout"
 import Card from '../../Components/Card'
-import { getProducts } from "../../Services/products.service";
-import { Product } from "../../Types/product";
 import ProductDetail from "../../Components/ProductDetail";
 
 function Home() {
-    const [items, setItems] = useState<Product[]>([]);
-
-    useEffect(() => {
-    const loadProducts = async () => {
-      try {
-        const products = await getProducts();
-        setItems(products);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        console.log(false);
-      }
-    };
-
-    loadProducts();
-  }, []);
+ 
+  const { items, search, setSearch } = useShoppingCar();
+  const filteredItems = items.filter(item => item.title.toLowerCase().includes(search.toLowerCase()));
 
   return (
       <Layout>
-        <div className="font-bold">Home</div>
-         <div className="grid grid-cols-4 gap-6 p-6">
-          {items.map(item => (
-            <Card key={item.id} data={item} />
-          ))}
+        <div className="flex items-center justify-center relative w-80 mb-4">
+          <h1 className="font-medium text-xl">Home</h1>
         </div>
+
+        <input 
+          type="text"
+          placeholder="Busca un producto"
+          className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)} 
+        />
+
+          <div className="grid grid-cols-4 gap-6 p-6">
+            {filteredItems.map(item => (
+              <Card key={item.id} data={item} />
+            ))}
+          </div>
+          
         <ProductDetail/>
       </Layout>
     )
